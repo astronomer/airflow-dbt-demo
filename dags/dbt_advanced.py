3c9ccd4c9914fdfdb6ce87f818bdd6e47c0f422e
Benjamin <denimalpaca@gmail.com>
2022-04-04 09:36:06 -0400

    Update dags/dbt_advanced.py
    Co-authored-by: Josh Fell <48934154+josh-fell@users.noreply.github.com>

--- dags/dbt_advanced.py
+++ dags/dbt_advanced.py
@@ -11,7 +11,7 @@ DBT_PROJECT_DIR = "/usr/local/airflow/dbt"
 with DAG(
     'dbt_advanced_dag',
     start_date=datetime(2020, 12, 23),
-    description='A dbt wrapper for airflow',
+    description='A dbt wrapper for Airflow.',
     schedule_interval=None,
     catchup=False,
 ) as dag:
