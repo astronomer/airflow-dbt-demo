c63b7e7f5e433e2d3fd46458f3a3ff049adee430
Faisal <fhoda@users.noreply.github.com>
2022-11-09 21:17:45 -0600

    Revert "move dbt dir from root to under include"

--- dags/dbt_advanced_utility.py
+++ dags/dbt_advanced_utility.py
@@ -13,7 +13,7 @@ from include.dbt_dag_parser import DbtDagParser
 
 # We're hardcoding these values here for the purpose of the demo, but in a production environment these
 # would probably come from a config file and/or environment variables!
-DBT_PROJECT_DIR = "include/dbt"
+DBT_PROJECT_DIR = "/usr/local/airflow/dbt"
 DBT_GLOBAL_CLI_FLAGS = "--no-write-json"
 DBT_TARGET = "dev"
 DBT_TAG = "tag_staging"
